ea7a5a8e9cd381b987c9102c4f1057d4912a6cf0
Jual Rana <contactjual@gmail.com>
2024-12-26 23:07:35 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// isset() function use for checking a variable is declare or not 
+
+$name = "Jual";
+
+echo isset($name); // 1 means true
+
+
+if (isset($name)){
+    echo "Hello";
+} else {
+    echo "Bye";
+}
+
+
+?>
